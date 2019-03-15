34f4033033e28f47a302cb4be52c9af4cc55e454
Hanbum Park <kese111@gmail.com>
2019-03-15 02:11:13 -0700

    added vim

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 chmod 755 mkcscope.sh
 sudo cp mkcscope.sh /usr/bin/
 
+sudo apt-get install vim
 sudo apt-get install tmux
 sudo apt-get install cscope
 sudo apt-get install exuberant-ctags 
